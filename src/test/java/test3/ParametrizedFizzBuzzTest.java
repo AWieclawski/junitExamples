97677dada9ae623f958ee37e544eb1b55e450dac
Andrzej Wieclawski <a_wieclawski@poczta.pl>
2019-03-03 19:26:48 +0100

    Add @MethodSource from  argumentsProvider

--- src/test/java/test3/ParametrizedFizzBuzzTest.java
+++ src/test/java/test3/ParametrizedFizzBuzzTest.java
@@ -3,6 +3,7 @@ package test3;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import test2.FizzBuzz;
@@ -32,7 +33,7 @@ public class ParametrizedFizzBuzzTest {
     }
 
 
-    @DisplayName("Should return Buzz if number is divisible by three")
+    @DisplayName("Should return Buzz if number is divisible by five")
     @ParameterizedTest(name = "{index} - value {0}")
     @MethodSource("dataProvider")
     public void shouldReturnBuzzWhenDivisableBy5(int number) {
@@ -44,6 +45,25 @@ public class ParametrizedFizzBuzzTest {
 
     private static Stream<Integer> dataProvider(){
         return Stream.of(5,20,110,205,10000);
+    }
+
+    @DisplayName("Should return expected value")
+    @ParameterizedTest(name = "{index} - for {0} should return {1}")
+    @MethodSource("argumentsProvider")
+    public void shouldReturnExpectedValue(int number, String expectedResult) {
+//        String expectedResult = "Buzz!";
+        String actualResult = fizzBuzz.play(number);
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    private static Stream<Arguments> argumentsProvider(){
+        return Stream.of(
+                Arguments.arguments(1,"1"),
+                Arguments.arguments(3,"Fizz!"),
+                Arguments.arguments(5,"Buzz!"),
+                Arguments.arguments(15,"FizzBuzz!")
+                );
     }
 
 }
