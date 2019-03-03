ed4430001424ee402a3cd0b460d56e9d32827103
Andrzej Wieclawski <a_wieclawski@poczta.pl>
2019-03-03 20:16:57 +0100

    Add ThrowExceptionForNagative in AsserJFizzBuzzTest

--- src/test/java/AssertjFizzBuzzTest.java
+++ src/test/java/AssertjFizzBuzzTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import test2.FizzBuzz;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class AssertjFizzBuzzTest {
@@ -10,34 +11,38 @@ public class AssertjFizzBuzzTest {
 
     @Test
     public void shouldReturnFizzWhen3() {
-
         assertThat(fizzBuzz.play(3))
+                .overridingErrorMessage("Something went wrong!")
                 .isEqualTo("Fizz!")
                 .isNotEmpty()
                 .isNotBlank()
                 .isNotEmpty();
-
     }
 
     @Test
     public void shouldReturnBuzzWhen5() {
 
         assertThat(fizzBuzz.play(5)).isEqualTo("Buzz!");
-
     }
 
     @Test
     public void shouldReturnFizzBuzzWhen15() {
 
         assertThat(fizzBuzz.play(15)).isEqualTo("FizzBuzz!");
-
     }
 
     @Test
     public void shouldReturnStringNumberWhen1() {
 
         assertThat(fizzBuzz.play(1)).isEqualTo("1");
+    }
 
+    @Test
+    public void shouldThrowExceptionForNegative(){
+        int number=-1;
+        assertThatThrownBy(()->fizzBuzz.play(number))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("Number cannot be equal 0 or negative");
     }
 
 }
